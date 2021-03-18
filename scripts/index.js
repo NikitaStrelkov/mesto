8b213bc21d50abcc33825ef308601ce25c49a2b6
Nikita Strelkov <dol2live@gmail.com>
2021-03-18 21:27:43 +0300

    9

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ let openPopupBtn = document.querySelector('.edit-button')
 let closePopupBtn = document.querySelector('.popup__close-button');
 let popupContent = document.querySelector('.popup__content');
 
-let saveBtn = document.querySelector('.form__button');
+let saveBtn = document.querySelector('.popup__button');
 
 function openPopup() {
     popup.classList.add('popup_visible');
@@ -39,15 +39,15 @@ popupContent.addEventListener('click', function(event) {
 
 
 // Находим поля формы в DOM
-let formElement = document.querySelector('.form__input-container');
+let formElement = document.querySelector('.popup__input-container');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
     // Получите значение полей jobInput и nameInput из свойства value
-    let nameInput = document.querySelector('.form__item_name').value;
-    let jobInput = document.querySelector('.form__item_job').value;
+    let nameInput = document.querySelector('.popup__item_name').value;
+    let jobInput = document.querySelector('.popup__item_job').value;
     // Выберите элементы, куда должны быть вставлены значения полей
     document.querySelector('.profile__subtitle').textContent = nameInput.value;
     document.querySelector('.profile__subtitle').textContent = jobInput.value;
