2b57baec5d9002d7f7e950a9a3e444516609b523
Nikita Strelkov <dol2live@gmail.com>
2021-04-01 19:26:10 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
 
-let popup = document.querySelector('.popup');
-let openPopupBtn = document.querySelector('.profile__button-edit')
-let closePopupBtn = document.querySelector('.popup__close-button');
+let popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeAdd = document.querySelector('.popup_type_add');
+
+let openPopupEditBtn = document.querySelector('.profile__button-edit');
+let closePopupEditBtn = document.querySelector('.popup__close-button_edit');
+
+const addCardBtn = document.querySelector('.profile__button-add');
+const closePopupAddBtn = document.querySelector('.popup__close-button_add');
+
 let popupContent = document.querySelector('.popup__content');
 
 let saveBtn = document.querySelector('.popup__button');
@@ -14,29 +20,43 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__item_type_name');
 let jobInput = document.querySelector('.popup__item_type_job');
 
-function openPopup() {
+function openPopupEdit() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;    
-    popup.classList.add('popup_visible');
+    popupTypeEdit.classList.add('popup_visible');
 }
-function closePopup() {
-    popup.classList.remove('popup_visible');
+function closePopupEdit() {
+    popupTypeEdit.classList.remove('popup_visible');
+}
+
+function openPopupAdd() {
+    popupTypeAdd.classList.add('popup_visible');
+}
+function closePopupAdd() {
+    popupTypeAdd.classList.remove('popup_visible');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    closePopup();
+    closePopupEdit();
 
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 }
 
-openPopupBtn.addEventListener('click', function(event){
-    openPopup();
+openPopupEditBtn.addEventListener('click', function(event){
+    openPopupEdit();
+});
+closePopupEditBtn.addEventListener('click', function(event){
+    closePopupEdit();
+    
 });
 
-closePopupBtn.addEventListener('click', function(event){
-    closePopup();
+addCardBtn.addEventListener('click', function(event){
+    openPopupAdd();
+});
+closePopupAddBtn.addEventListener('click', function(event){
+    closePopupAdd();
 });
 
 popupContent.addEventListener('click', function(event) {
@@ -44,32 +64,5 @@ popupContent.addEventListener('click', function(event) {
 }); 
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
 
 
