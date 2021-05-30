d3b0bfa6f8630538a15be491f7a6cc3d27dc5921
Nikita Strelkov <dol2live@gmail.com>
2021-05-30 14:43:29 +0300

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import FormValidator from '../scripts/components/FormValidator.js';
 import {settingsForm} from '../scripts/utils/settingsForm.js';
 import Card from '../scripts/components/Card.js';
 import '../pages/index.css';
+import Api from '../scripts/components/Api.js';
 import {
   popupForm,
   popupAddForm,
   popupAddSaveButton,
+  popupButtonClose,
   popupFullImage,
   popupFullImageImage,
   popupFullImageTitle,
@@ -23,13 +25,23 @@ import {
   popupAddOpenButton,
   popupAddSelector,
   popupAddCloseButtonSelector,
-  inputErrorSelector
+  inputErrorSelector,
+  avatarImage,
+  popupAvatar,
+  popupAvatarButton,
+  popupAvatarForm,
+  popupAvatarInput,
+  popupAvatarSubmitButton,
+  popupAvatarCloseButton,
+  popupConfirmSelector,
+  popupDeleteIcon
 } from '../scripts/utils/constants.js';
 import PopupWithForm from '../scripts/components/PopupWithForm.js';
 import PopupWithImage from '../scripts/components/PopupWithImage.js';
 import UserInfo from '../scripts/components/UserInfo.js';
 import Section from '../scripts/components/Section.js';
-import {initialCards} from '../scripts/utils/utils.js';
+// import {initialCards} from '../scripts/utils/utils.js';
+import PopupWithSubmit from '../scripts/components/PopupWithSubmit.js'
 
 // Открытие попапа редактирования профиля
 const userInfo = new UserInfo(profileSelectors);
@@ -46,8 +58,11 @@ const formSubmitHandler = (data) => {
     name: data['name'],
     profession: data['profession']
   }
-  userInfo.setUserInfo(info);
-  popupEditProfile.close();
+  api.editUserInfo(info.name, info.profession)
+    .finally(() => {
+      userInfo.setUserInfo(info);
+      popupEditProfile.close();
+    })
 }
 
 // Открытие попапа добавление карточки
@@ -57,7 +72,7 @@ popupAddOpenButton.addEventListener('click', function() {
 
 // Обработчик добавления карточки
 const formSubmitAddHandler = (data) => {
-  const dataCard = { 
+  const dataCard = {
     name: data['title-card'],
     link: data['link-card']
   }
@@ -67,7 +82,37 @@ const formSubmitAddHandler = (data) => {
   popupAddForm.reset();
 }
 
-  // Рендеринг
+// Открытие попапа подтверждение удаления карточки
+
+// Открытие попапа изменение аватара
+popupAvatarButton.addEventListener('click', function() {
+  popupEditAvatar.open();
+  popupEditAvatar.resetWaitSubmitButton();
+});
+
+const formDeleteSubmitHandler = (evt, card) => {
+  evt.preventDefault();
+  api.deleteCard()
+}
+
+// Обработчик попапа изменение аватара
+const formEditAvatarSubmitHandler = (e) => {
+  e.preventDefault();
+  avatarImage.src = popupAvatarInput.value;
+  popupEditAvatar.waitSubmitButton('Сохранение...');
+  popupEditAvatar.close();
+}
+
+// Работа с API
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-24',
+  headers: {
+    authorization: '2373054b-c310-4707-af21-19b2a17fc69f',
+    'Content-Type': 'application/json'
+  }
+});
+
+// Рендеринг
 function renderCard(card) {
   initialSection.addItem(card);
 }
@@ -80,17 +125,27 @@ const createCard = (card) => {
   return new Card(card, '#grid-template', handleCardClick)
 
 }
-  const initialSection = new Section({items: initialCards, 
-  renderer: (item) => {
-    const card = createCard(item);
-    initialSection.addItem(card.getCard());
-  }}, photoCard)
+api.getInitialCards().then((cards) => {
+  generateInitialCard(cards)
+})
+const generateInitialCard = (cards) => {
+  const initialSection = new Section({items: cards,
+    renderer: (item) => {
+      const card = createCard(item);
+      initialSection.addItem(card.getCard());
+    }}, photoCard)
   initialSection.renderItems();
+}
 
 
 // Включаем валидацию формы редактрования профиля
 const editFormValidator = new FormValidator(settingsForm, popupForm, inputErrorSelector);
 editFormValidator.enableValidation();
+
+// Попап редактирования аватара
+const popupEditAvatar = new PopupWithSubmit(profileSelectors.profileAvatarSelector, popupAvatarCloseButton,
+  formEditAvatarSubmitHandler);
+popupEditAvatar.setEventListeners();
 
 // Включаем валидацию формы добавления карточки
 const addFormValidator = new FormValidator(settingsForm, popupAddForm, inputErrorSelector);
@@ -109,6 +164,14 @@ popupEditProfile.setEventListeners();
 const popupAddCard = new PopupWithForm(popupAddSelector, popupAddCloseButtonSelector,
   formSubmitAddHandler)
 popupAddCard.setEventListeners();
+
+// Попап подтвеждения удаления
+const popupConfirm = new PopupWithSubmit(popupConfirmSelector, popupAddCloseButtonSelector,
+  (evt, card) => {
+    formDeleteSubmitHandler(evt, card)
+  }
+)
+popupConfirm.setEventListeners();
 
 export {
   popupFullImage,
